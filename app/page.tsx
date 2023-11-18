a4a23ed328c2f7be5b47ee1291d1ddd23160a208
aashuking <aashutoshkumar19@ece.iiitp.ac.in>
2023-11-18 23:47:53 +0530

    herocarousel done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,38 @@
+"use client"
 import React from 'react'
+import Image from 'next/image'
+import Searchbar from '@/components/Searchbar'
+import HeroCarousel from '@/components/HeroCarousel'
 
 const Home = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+    <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <div className="flex max-xl:flex-col gap-16">
+        <div className="flex flex-col justify-center">
+          <p className="small-text">
+            Smart Shopping Starts Here:
+            <Image
+            src="assets/icons/arrow-right.svg"
+            alt="arrow-right"
+            width={16}
+            height={16}></Image>
+
+            
+          </p>
+          <h1 className="head-text">Unleash the power of <span className="text-primary">PriceTracker</span></h1>
+          <p className="mt-6">
+            Powerful,self-serve and growth analytics to help you convert,engage, and retain more.
+          </p>
+
+          <Searchbar/>
+        </div>
+        <HeroCarousel/>
+      </div>
+
+      
+    </section>
+    </>
   )
 }
 
---
+++ components/Searchbar.tsx
@@ -0,0 +1,47 @@
+"use client"
+import React, { FormEvent,useState } from 'react'
+
+const isValidAmazonProductURL = (url:string)=>{
+    try{
+        const parsedURL = new URL(url);
+        const hostname= parsedURL.hostname;
+
+        if(hostname.includes('amazon.in')|| hostname .includes('amazon.') || hostname.endsWith('amazon')){
+            return true;
+        }
+    }catch(error){
+        return false;
+    }
+    return false;
+}
+
+
+
+const Searchbar = () => {
+    const[searchPrompt,setsearchPrompt]= useState('');
+    const[isLoading,setIsLoading]= useState(false);
+    const handleSubmit=(event:FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const  isValidLink= isValidAmazonProductURL(searchPrompt);
+        if(!isValidLink)return alert('Please enter a valid Amazon product Link');
+        try{
+            setIsLoading(true);
+        }catch(error){
+            console.log(error);
+        }finally{
+            setIsLoading(false);
+        }
+
+    }
+    return (
+        <form
+        onSubmit={handleSubmit}
+        action="" className="flex flex-wrap gap-4 mt-12">
+            <input value={searchPrompt} onChange={(e)=> setsearchPrompt(e.target.value)} type="text" placeholder="Enter product link"
+            className="searchbar-input" />
+            <button  disabled={searchPrompt ===''} type="submit" className="searchbar-btn">{isLoading ?'Searching':'Search'}</button>
+        </form>
+    )
+}
+
+export default Searchbar
