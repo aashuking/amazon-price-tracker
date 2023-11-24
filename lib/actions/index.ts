03711c046230e8b6255f674340282688643bbcfa
aashuking <aashutoshkumar19@ece.iiitp.ac.in>
2023-11-25 04:06:52 +0530

    scraped data is stored in db

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,18 +1,50 @@
 "use server"
 
-
-import { connectToDB } from "@/lib/mongoose";
+import { revalidatePath } from "next/cache";
+import Product from "../models/product.model";
+import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
+// import { generateEmailBody, sendEmail } from "../nodemailer";
 
-export async function scrapeAndStoreProduct(productUrl: string){
-    if(!productUrl)return ;
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if(!productUrl) return;
 
-    try{
-        connectToDB();
+  try {
+    connectToDB();
 
-        const scrapedProduct = await scrapeAmazonProduct(productUrl);
-        if(!scrapedProduct)return ;
-    }catch(error:any){
-        throw new Error(`Failed to create/update product : ${error.message}`)
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+    if(!scrapedProduct) return;
+
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice }
+      ]
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
     }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`)
+  }
 }
